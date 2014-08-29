b9a24f13a14806567c088c160500559b5a3971ae
bbukacek <bbukacek@hortonworks.com>
2014-08-29 16:11:51 -0500

    modified run.sh and tear-down.sh::modifications to overall demo

--- run.sh
+++ run.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
 #Create Hive Table
-su hdp -c 'hive -f hive/apache_logs.hql'
+hive -f hive/apache_logs.hql
 
 #Application and Falcon Setup
-su hdfs -c 'hadoop fs -mkdir -p /apps/falcon/lib'
-su hdfs -c 'hadoop fs -mkdir -p /apps/falcon/pig'
-su hdfs -c 'hadoop fs -copyFromLocal lib/*.jar /apps/falcon/lib'
-su hdfs -c 'hadoop fs -copyFromLocal /usr/lib/hbase/lib/*.jar /apps/falcon/lib'
-su hdfs -c 'hadoop fs -copyFromLocal pig/*.pig /apps/falcon/pig'
+hadoop fs -mkdir -p /apps/apache_logs/lib
+hadoop fs -mkdir -p /apps/apache_logs/pig
+hadoop fs -copyFromLocal lib/*.jar /apps/apache_logs/lib
+hadoop fs -copyFromLocal /usr/lib/hbase/lib/*.jar /apps/apache_logs/lib
+hadoop fs -copyFromLocal pig/*.pig /apps/apache_logs/pig
 
-su hdp -c 'falcon entity -type cluster -submit -name local-cluster -file falcon/local_cluster.xml'
-su hdp -c 'falcon entity -type feed -submit -name logDataIn -file falcon/log_data_in-feed.xml'
-su hdp -c 'falcon entity -type feed -submit -name logDataOut -file falcon/log_data_out-feed.xml'
-su hdp -c 'falcon entity -type process -submit -name log-load -file falcon/process.xml'
-su hdp -c 'faclon entity -type process -name log-load -schedule'
+falcon entity -type cluster -submit -name demo-cluster -file falcon/cluster.xml
+falcon entity -type feed -submit -name logDataIn -file falcon/log_data_in-feed.xml
+falcon entity -type feed -submit -name logDataOut -file falcon/log_data_out-feed.xml
+falcon entity -type process -submit -name log-load -file falcon/process.xml
+faclon entity -type process -name log-load -schedule
+
+mkdir -p ~/demo-output/flume
 
 #Start Flume
-su hdp -c '/usr/lib/flume/bin/flume-ng agent -n agent -c conf -f /home/hdp/demo-web-logs/flume/flume-onaam.conf -Dflume.root.logger=INFO,console &'
+#/usr/lib/flume/bin/flume-ng agent -n agent -c conf -f flume/flume-onaam.conf
 
 #Compile and Run Application
 mvn compile
--- tear-down.sh
+++ tear-down.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 
-su hdp -c 'falcon entity -type process -name log-load -delete'
-su hdp -c 'falcon entity -type feed -name logDataOut -delete'
-su hdp -c 'falcon entity -type feed -name logDataIn -delete'
-su hdp -c 'falcon entity -type cluster -name local-cluster -delete'
+falcon entity -type process -name log-load -delete
+falcon entity -type feed -name logDataOut -delete
+falcon entity -type feed -name logDataIn -delete
+falcon entity -type cluster -name local-cluster -delete
 
-
-su hdfs -c 'hadoop fs -rm -r /user/hdp/logs'
-su hdfs -c 'hadoop fs -rm -r /apps/falcon'
+hadoop fs -rm -r /user/hdp/logs
+hadoop fs -rm -r /apps/apache_logs
